41b360f313c0056f60ecacd80586ea0d5cbdc906
xooxeedev <lmowngml@naver.com>
2020-10-30 17:41:51 +0900

    test

---
+++ 0_개발환경 세팅.sql
@@ -0,0 +1,6 @@
+-- 한 줄 주석 
+/* 범위 주석 */
+
+-- EMPLOYEE 테이블에 있는 모든 사원 조회
+-- ' * ' : 모든, 전체 (ALL) 
+SELECT * FROM EMPLOYEE;
---
+++ sys as sysdba_관리자.sql
@@ -0,0 +1,6 @@
+select username from dba_users;
+
+
+CREATE USER kh IDENTIFIED BY kh;
+
+GRANT RESOURCE, CONNECT TO kh;
